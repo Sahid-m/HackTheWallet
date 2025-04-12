c2b7c29d0178344ab6379a96b69eaec6d9ffd288
Zohaib Ehtehsam <zohaib.ehtesham@gmail.com>
2025-04-12 12:05:00 +0100

    Wallet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import dynamic from 'next/dynamic';
+import { useBalance, useAccount } from "@starknet-react/core";
+const WalletBar = dynamic(() => import('../components/wallet-bar'), { ssr: false })
+
 
 export default function Home() {
+  const { address, status } = useAccount();
+  const { data, error } = useBalance({
+  address: address,
+});
   return (
+    <div>
     <Button>
       Hello World
     </Button>
+    {data?.formatted}
+    {status}
+    <WalletBar />
+  </div>
   );
 }
+
